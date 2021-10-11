008b46d53f9aacc656a3443f0124ec7cb9ce2b20
José Lescano <92216756+jlesca@users.noreply.github.com>
2021-10-11 11:12:00 -0300

    Create values-boleans.py

---
+++ values-boleans.py
@@ -0,0 +1,6 @@
+# VALORES BOOLEANOS EN PYTHON
+# En Python podemos usar operadores relacionales para comparar dos valores.
+
+print(10 > 9) # Mostrará True
+print(10 == 9) # Mostrará False
+print(10 < 9) # Mostrará False
