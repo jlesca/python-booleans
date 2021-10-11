073bb7ca358c787ebfe48577c1f7c72bbb5db55b
José Lescano <92216756+jlesca@users.noreply.github.com>
2021-10-11 11:35:51 -0300

    Update values-booleans.py

--- values-booleans.py
+++ values-booleans.py
@@ -4,3 +4,13 @@
 print(10 > 9) # Mostrará True
 print(10 == 9) # Mostrará False
 print(10 < 9) # Mostrará False
+
+
+# Podemos hacer uso de un IF para comparar dos valores y ejecutar una instrucción si ese IF se cumple.
+
+a = 10 # Creo la variable y su valor int
+b = 9 # Creo la variable y su valor int
+if a > b: # Si se cumple que A es mayor que B, que haga lo siguiente:
+  print("A is greater than B") # Que imprima este mensaje.
+else: # Si no se cumple, que haga lo siguiente:
+  print("B is greater than A") # Que muestre este mensaje.
